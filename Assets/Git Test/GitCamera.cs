63a8363a8313a7d51567fbaefc5e21b2a3916c7a
Lostage3 <lostage30030@gmail.com>
2020-06-30 14:02:49 +0200

    CameraControl

---
+++ Assets/Git Test/GitCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitCamera : MonoBehaviour
+{
+    public float offsetY = 5;
+    public float offsetZ = 10;
+    public GameObject player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, player.transform.position.z - offsetZ);
+    }
+}
